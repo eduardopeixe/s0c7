645ef48582b33d0d390d4464aabc7a7d1ebda6e8
Eduardo Peixe <eduardo.peixe@indexexchange.com>
2020-10-04 14:41:50 -0400

    Navbar ready

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core'
 
 import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
