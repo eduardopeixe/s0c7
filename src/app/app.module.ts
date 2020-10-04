2b38026217fedc45aace46542148d14ac947e3f5
Eduardo Peixe <eduardo.peixe@indexexchange.com>
2020-10-04 12:26:24 -0400

    Signup ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule } from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { QuestionsComponent } from './questions/questions.component';
-import { AskComponent } from './questions/ask/ask.component';
-import { AnswerComponent } from './questions/answer/answer.component';
-import { ReadComponent } from './questions/read/read.component';
-import { UsersComponent } from './users/users.component';
-import { ProfileComponent } from './users/profile/profile.component';
-import { TagsComponent } from './tags/tags.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from './material.module'
+import { SignupComponent } from './auth/signup/signup.component'
+import { LoginComponent } from './auth/login/login.component'
+import { QuestionsComponent } from './questions/questions.component'
+import { AskComponent } from './questions/ask/ask.component'
+import { AnswerComponent } from './questions/answer/answer.component'
+import { ReadComponent } from './questions/read/read.component'
+import { UsersComponent } from './users/users.component'
+import { ProfileComponent } from './users/profile/profile.component'
+import { TagsComponent } from './tags/tags.component'
 import { WelcomeComponent } from './welcome/welcome.component'
 
 @NgModule({
@@ -28,15 +30,17 @@ import { WelcomeComponent } from './welcome/welcome.component'
     UsersComponent,
     ProfileComponent,
     TagsComponent,
-    WelcomeComponent
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
+
+import { MatButtonModule } from '@angular/material/button'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
 
 @NgModule({
-  imports: [MatFormFieldModule, MatInputModule],
-  exports: [MatFormFieldModule, MatInputModule]
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatFormFieldModule, MatInputModule],
 })
-export class MaterialModule {
-}
+export class MaterialModule {}
